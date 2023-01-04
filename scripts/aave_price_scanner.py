0e835921991637c7a56f39393b0b09f741b4b4f1
Thomas <tombutler4@hotmail.com>
2023-01-03 19:40:52 -0600

    basic aave price query

--- scripts/aave_price_scanner.py
+++ scripts/aave_price_scanner.py
@@ -1,8 +1,8 @@
-from brownie import Contract, network, config
+from brownie import Contract, network, config, interface
+
+ORACLE = config["networks"][network.show_active()]["AAVE_ORACLE"]
+WETH = config["networks"][network.show_active()]["WETH_TOKEN"]
 
 def main():
-    oracle_address = config["networks"][network.show_active()]["AAVE_ORACLE"]
-    #oracle_abi = open("abis/AaveOracle.txt", "r", encoding='utf-8').read()
-    #print(oracle_abi)
-    #oracle = Contract.from_abi("AaveOracle", oracle_address, oracle_abi)
-    oracle = Contract(oracle_address)
+    oracle = interface.IPriceOracle(ORACLE)
+    print(oracle.getAssetPrice(WETH))
