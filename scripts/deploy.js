45b330d2b4f2d4e2a65e7a4061f9f5ea7331b93e
Thomas <tombutler4@hotmail.com>
2023-01-18 22:38:49 -0600

    added helper config to store addresses

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,9 @@
-
+const { networkConfig } = require("./helper-hardhat-config");
 const hre = require("hardhat");
 
 async function main() {
     // deploy contract
-    const poolAddressesProvider = "0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb";
+    const poolAddressesProvider = networkConfig[hre.network.config.chainId].aavePoolAddressesProvider;
     const FlashLoanArbitrage = await hre.ethers.getContractFactory("FlashLoanArbitrage");
     const flashLoanArbitrage = await FlashLoanArbitrage.deploy(poolAddressesProvider);
     await flashLoanArbitrage.deployed();
